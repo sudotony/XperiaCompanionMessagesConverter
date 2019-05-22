2e8807ba77e1cca3c81a1314c320a90f4acceee4
Tony Nguyen <tony.nguyen@oakton.com.au>
2019-05-22 20:57:52 +1000

    Add project files.

---
+++ XperiaCompanionMessagesConverter/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XperiaCompanionMessagesConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (File.Exists("messages.xml"))
+            {
+                // XT XML VERSION=00000001
+                XmlDocument collection = new XmlDocument();
+
+                try
+                {
+                    collection.Load("messages.xml");
+                    XmlNodeList messages = collection.GetElementsByTagName("sms");
+
+                    XmlDocument smsesDoc = new XmlDocument();
+                    XmlDeclaration xmlDeclaration = smsesDoc.CreateXmlDeclaration("1.0", "UTF-8", "yes");
+                    XmlElement root = smsesDoc.DocumentElement;
+                    smsesDoc.InsertBefore(xmlDeclaration, root);
+
+                    XmlElement smses = smsesDoc.CreateElement(string.Empty, "smses", string.Empty);
+                    smses.SetAttribute("count", messages.Count.ToString());
+
+                    Guid backupSet = new Guid();
+                    smses.SetAttribute("backup_set", backupSet.ToString());
+                    smses.SetAttribute("backup_date", Extensions.ToUnixTime(DateTime.Now).ToString());
+
+                    for (int i = 0; i < messages.Count; i++)
+                    {
+                        Console.Write("\rLine " + (i + 1) + " out of " + messages.Count);
+
+                        XmlElement sms = smsesDoc.CreateElement("sms");
+
+                        // default values
+                        sms.SetAttribute("date_sent", "0");
+                        sms.SetAttribute("locked", "0");
+                        sms.SetAttribute("protocol", "0");
+                        sms.SetAttribute("read", "1");
+                        sms.SetAttribute("sc_toa", "null");
+                        sms.SetAttribute("service_center", "null");
+                        sms.SetAttribute("status", "-1");
+                        sms.SetAttribute("subject", "null");
+                        sms.SetAttribute("toa", "null");
+
+                        // values from messages.xml
+                        sms.SetAttribute("address", messages[i]["address"].InnerText);
+                        sms.SetAttribute("date", messages[i]["timestamp"].InnerText);
+                        sms.SetAttribute("type", messages[i]["type"].InnerText.Equals("incoming") ? "1" : "2");
+                        sms.SetAttribute("body", messages[i]["text"].InnerText);
+
+                        // don't know if i need readable_date
+                        //sms.SetAttribute("readable_date", Extensions.UnixTimeStampToDateTime(double.Parse(messages[i]["timestamp"].InnerText)).ToString("d MMMM HH:mm:ss"));
+                        // don't know if i have contact name in messages.xml
+                        //sms.SetAttribute("contact_name", "");
+
+                        smses.AppendChild(sms);
+                    }
+
+                    smsesDoc.AppendChild(smses);
+
+                    //smsesDoc.OuterXml
+                    smsesDoc.Save("sms-" + DateTime.Now.ToString("yyyyMMddhhmmss") + ".xml");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception caught.");
+                    Console.WriteLine("Could be that you have XT XML VERSION=00000001 at the top of messages.xml. Delete that line and try again.");
+                    Console.WriteLine("The error message:");
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("Press any key to finish.");
+                    Console.ReadKey();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Could not find messages.xml.");
+                Console.WriteLine("Please place the messages.xml file from Sony Xperia Companion Backups in the same directory as the executable.");
+                Console.WriteLine("Press any key to finish.");
+                Console.ReadKey();
+            }
+        }
+    }
+
+    public static class Extensions
+    {
+        // courtesy of https://stackoverflow.com/questions/24837474/c-sharp-convert-dates-to-timestamp
+        public static double ToUnixTime(this DateTime input)
+        {
+            return input.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+        }
+
+        // courtesy of https://stackoverflow.com/questions/249760/how-can-i-convert-a-unix-timestamp-to-datetime-and-vice-versa
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
+        }
+
+    }
+}
---
+++ XperiaCompanionMessagesConverter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("XperiaCompanionMessagesConverter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XperiaCompanionMessagesConverter")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8a08b5b1-0942-470a-9ab2-bd0717bdadb0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
